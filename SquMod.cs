84743e9cc41bb33aecb7a59b8049ee797817c26e
Sadamune <zwiebelshi@gmail.com>
2026-06-20 15:34:00 +0900

    add dependencies: RitsuLib

--- SquMod.cs
+++ SquMod.cs
@@ -1,13 +1,25 @@
+using System.Reflection;
 using MegaCrit.Sts2.Core.Logging;
 using MegaCrit.Sts2.Core.Modding;
+using STS2RitsuLib;
+using STS2RitsuLib.Interop;
 
 namespace Squ;
 
-[ModInitializer("ModLoaded")]
+[ModInitializer(nameof(ModLoaded))]
 public static class SquMod
 {
+	public const string ModId = "sunqian-universe";
+
+	public static Logger Logger { get; private set; } = null!;
+
 	public static void ModLoaded()
 	{
-		Log.Warn("sunqian-universe (SQU) mod loaded!");
+		var assembly = Assembly.GetExecutingAssembly();
+
+		Logger = RitsuLibFramework.CreateLogger(ModId);
+		ModTypeDiscoveryHub.RegisterModAssembly(ModId, assembly);
+
+		Logger.Info("sunqian-universe (SQU) mod loaded!");
 	}
 }
