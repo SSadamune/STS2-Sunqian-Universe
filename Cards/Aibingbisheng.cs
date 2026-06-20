d0bc0063773d6755964452876f7d4b0d5a8cd871
Sadamune <zwiebelshi@gmail.com>
2026-06-20 16:33:22 +0900

    add Aibingbisheng

---
+++ Cards/Aibingbisheng.cs
@@ -0,0 +1,69 @@
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using MegaCrit.Sts2.Core.Commands;
+using MegaCrit.Sts2.Core.Entities.Cards;
+using MegaCrit.Sts2.Core.GameActions.Multiplayer;
+using MegaCrit.Sts2.Core.HoverTips;
+using MegaCrit.Sts2.Core.Localization.DynamicVars;
+using MegaCrit.Sts2.Core.Models.CardPools;
+using MegaCrit.Sts2.Core.Models.Powers;
+using STS2RitsuLib.Combat.CardTargeting;
+using STS2RitsuLib.Interop.AutoRegistration;
+using STS2RitsuLib.Scaffolding.Content;
+
+namespace Squ.Cards;
+
+[RegisterCard(typeof(ColorlessCardPool))]
+public sealed class Aibingbisheng : ModCardTemplate
+{
+	protected override IEnumerable<DynamicVar> CanonicalVars =>
+	[
+		new PowerVar<StrengthPower>(-8),
+		new PowerVar<RitualPower>(3),
+	];
+
+	protected override IEnumerable<IHoverTip> AdditionalHoverTips =>
+	[
+		HoverTipFactory.FromPower<StrengthPower>(),
+		HoverTipFactory.FromPower<RitualPower>(),
+	];
+
+	public override CardAssetProfile AssetProfile => new(
+		PortraitPath: "res://images/cards/Aibingbisheng.png");
+
+	public override IEnumerable<CardKeyword> CanonicalKeywords =>
+	[
+		CardKeyword.Exhaust,
+	];
+
+	public Aibingbisheng()
+		: base(1, CardType.Skill, CardRarity.Common, CustomTargetType.Anyone)
+	{
+	}
+
+	protected override async Task OnPlay(PlayerChoiceContext choiceContext, CardPlay cardPlay)
+	{
+		ArgumentNullException.ThrowIfNull(cardPlay.Target, "cardPlay.Target");
+
+		await PowerCmd.Apply<StrengthPower>(
+			choiceContext,
+			cardPlay.Target,
+			DynamicVars[nameof(StrengthPower)].BaseValue,
+			Owner.Creature,
+			this);
+
+		await PowerCmd.Apply<RitualPower>(
+			choiceContext,
+			cardPlay.Target,
+			DynamicVars[nameof(RitualPower)].BaseValue,
+			Owner.Creature,
+			this);
+	}
+
+	protected override void OnUpgrade()
+	{
+		AddKeyword(CardKeyword.Retain);
+		AddKeyword(CardKeyword.Innate);
+	}
+}
