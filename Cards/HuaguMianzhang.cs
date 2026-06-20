259b5b239a8ae1ff242ec0520e4539280902dff2
Sadamune <zwiebelshi@gmail.com>
2026-06-21 00:28:10 +0900

    add Huagumianzhang

---
+++ Cards/HuaguMianzhang.cs
@@ -0,0 +1,71 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using MegaCrit.Sts2.Core.Commands;
+using MegaCrit.Sts2.Core.Commands.Builders;
+using MegaCrit.Sts2.Core.Entities.Cards;
+using MegaCrit.Sts2.Core.Entities.Creatures;
+using MegaCrit.Sts2.Core.GameActions.Multiplayer;
+using MegaCrit.Sts2.Core.HoverTips;
+using MegaCrit.Sts2.Core.Localization.DynamicVars;
+using MegaCrit.Sts2.Core.Models.CardPools;
+using MegaCrit.Sts2.Core.ValueProps;
+using Squ.Powers;
+using STS2RitsuLib.Interop.AutoRegistration;
+using STS2RitsuLib.Scaffolding.Content;
+
+namespace Squ.Cards;
+
+[RegisterCard(typeof(ColorlessCardPool))]
+public sealed class HuaguMianzhang : ModCardTemplate
+{
+	protected override IEnumerable<DynamicVar> CanonicalVars =>
+	[
+		new DamageVar(12m, ValueProp.Move),
+	];
+
+	protected override IEnumerable<IHoverTip> AdditionalHoverTips =>
+	[
+		HoverTipFactory.FromPower<HuaguMianzhangPower>(),
+	];
+
+	public override CardAssetProfile AssetProfile => new(
+		PortraitPath: "res://images/cards/HuaguMianzhang.png");
+
+	public HuaguMianzhang()
+		: base(2, CardType.Attack, CardRarity.Rare, TargetType.AnyEnemy)
+	{
+	}
+
+	protected override async Task OnPlay(PlayerChoiceContext choiceContext, CardPlay cardPlay)
+	{
+		ArgumentNullException.ThrowIfNull(cardPlay.Target, "cardPlay.Target");
+
+		AttackCommand attack = await DamageCmd.Attack(DynamicVars.Damage.BaseValue)
+			.FromCard(this)
+			.Targeting(cardPlay.Target)
+			.WithHitFx("vfx/vfx_attack_blunt")
+			.Execute(choiceContext);
+
+		int damageDealt = attack.Results
+			.SelectMany(results => results)
+			.Where(result => result.Receiver == cardPlay.Target)
+			.Sum(result => result.TotalDamage);
+
+		if (damageDealt > 0 && cardPlay.Target.IsAlive)
+		{
+			await PowerCmd.Apply<HuaguMianzhangPower>(
+				choiceContext,
+				cardPlay.Target,
+				damageDealt,
+				Owner.Creature,
+				this);
+		}
+	}
+
+	protected override void OnUpgrade()
+	{
+		DynamicVars.Damage.UpgradeValueBy(4m);
+	}
+}
